36fbc767099ef303bf933af11baf239f1d214157
Patrick Corcoran <patrickcorcoran10@gmail.com>
2023-06-13 21:03:03 -0500

    seeding attempt

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -5,6 +5,31 @@ require("dotenv").config();
 let pool
 if (process.env.JAWSDB_URL) {
     pool = mysql.createPool(process.env.JAWSDB_URL)
+    pool.getCoonection(() => {
+        connection.query(`
+        USE user_management_db;
+    
+    CREATE TABLE Users (
+        id int not null auto_increment,
+        first_name varchar(20),
+        last_name varchar(20),
+        email varchar(30),
+        phone varchar(20),
+        comments text,
+        status varchar(20) not null default 'active', 
+        primary key (id)
+    );
+    
+    INSERT INTO Users
+    VALUES (NULL, 'Jacob', 'Lee', 'alais@email.com', "815-555-4321", 'What a cool dude', 'active'),
+    (NULL, 'Tyson', 'Fury', 'tyson@email.com', '815-111-4321', 'What a fighter', 'active'),
+    (NULL, 'Mohammed', 'Ali', 'ali@email.com', '815-222-4321', 'Float like a butterfly', 'active'),
+    (NULL, 'Cider', 'Goad', 'cider@email.com', '815-333-4321', 'What a cool dog', 'active'),
+    (NULL, 'Novie', 'Gracie', 'nova@email.com', '815-444-4321', 'What a cool cat', 'active'),
+    (NULL, 'Raindrop', 'Pinkston', 'raindrop@email.com', '815-777-4321', 'This thing is evil', 'active');
+        `)
+    })
+    
 } else {
     pool = mysql.createPool({
         connectionLimit : 100,
@@ -15,22 +40,8 @@ if (process.env.JAWSDB_URL) {
     });
 }
 
-// let db;
-// if (process.env.JAWSDB_URL) {
-//     console.log('hello patrick')
-//     db = mysql.createConnection(process.env.JAWSDB_URL)
-// } else {
-//     console.log('hi pat')
-//     db = mysql.createConnection(
-//         {
-//           host: 'localhost',
-//           user: process.env.DB_USER,
-//           password: process.env.DB_PASSWORD,
-//           database: process.env.DB_NAME
-//         },
-//         console.log(`Connected to the movies_db database.`)
-//       );
-// }
+
+
 
 // View Users
 exports.view = (req, res) => {
