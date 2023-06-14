a6c3f6fee2f1e12173ddbd9c66ceb654547dcbb9
Patrick Corcoran <patrickcorcoran10@gmail.com>
2023-06-13 20:37:26 -0500

    commit with new thingy

--- app.js
+++ app.js
@@ -27,18 +27,36 @@ app.engine("hbs", exphbs.engine( {extname: ".hbs" }));
 app.set("view engine", "hbs");
 
 // Connection Pool
-const pool = mysql.createPool({
-    connectionLimit : 100,
-    host: process.env.JAWSDB_URL,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-});
+// const pool = mysql.createPool({
+//     connectionLimit : 100,
+//     host: process.env.JAWSDB_URL,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME
+// });
 
-pool.getConnection((err, connection) => {
-    if(err) throw err; //not connect
-    console.log("connected as ID " + connection.threadId);
-})
+
+// pool.getConnection((err, connection) => {
+//     if(err) throw err; //not connect
+//     console.log("connected as ID " + connection.threadId);
+// })
+let db;
+if (process.env.JAWSDB_URL) {
+    db = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+    db = mysql.createConnection(
+        {
+          host: 'localhost',
+          // MySQL username,
+          user: process.env.DB_USER,
+          // TODO: Add MySQL password here
+          password: process.env.DB_PASSWORD,
+          database: process.env.DB_NAME
+        },
+        console.log(`Connected to the movies_db database.`)
+      );
+}
+
 
 const routes = require("./server/routes/user");
 app.use("/", routes);
